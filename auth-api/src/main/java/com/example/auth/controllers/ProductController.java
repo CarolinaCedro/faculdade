a1ef743f64bce5d2f3746a11a495133eefc7372a
CarolinaCedro <yotechgirl@gmail.com>
2023-09-05 22:04:16 -0300

    feat: atividade autentificação

---
+++ auth-api/src/main/java/com/example/auth/controllers/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.auth.controllers;
+
+import com.example.auth.domain.product.Product;
+import com.example.auth.domain.product.ProductRequestDTO;
+import com.example.auth.domain.product.ProductResponseDTO;
+import com.example.auth.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//criei esse controller para adicionar uma nova interatividade no sistema e conseguir fazer uma filtragem de produtos por exemplo
+
+@RestController()
+@RequestMapping("product")
+public class ProductController {
+
+    @Autowired
+    ProductRepository repository;
+
+    @PostMapping
+    public ResponseEntity postProduct(@RequestBody @Valid ProductRequestDTO body){
+        Product newProduct = new Product(body);
+
+        this.repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        List<ProductResponseDTO> productList = this.repository.findAll().stream().map(ProductResponseDTO::new).toList();
+
+        return ResponseEntity.ok(productList);
+    }
+}
---
+++ auth-api/src/main/resources/db/migration/V1__create-product-table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE product (
+                         id VARCHAR(255) PRIMARY KEY NOT NULL,
+                         name VARCHAR(255) NOT NULL,
+                         price INT NOT NULL
+);
